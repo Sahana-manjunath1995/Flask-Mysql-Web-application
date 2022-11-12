d0e97f9e41a6ed4965469fe7ab98a30cd812c140
root <root@DESKTOP-MIJ4IO3.localdomain>
2022-11-12 13:00:34 +0530

    config-code

--- app/config.py
+++ app/config.py
@@ -9,7 +9,6 @@ def connect():
     return mysql.connector.connect(host = config['mysql']['host'],
                                    user = config['mysql']['user'],
                                    passwd = config['mysql']['password'],
-                                 #    port_no = config['mysql'][port],
                                    db = config['mysql']['database'])
 
 
